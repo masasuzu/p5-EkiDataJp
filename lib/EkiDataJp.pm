b65e3ceae094f571f7ff356dd261f7d48f6333b5
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2012-10-01 09:12:50 +0900

    initial commit

---
+++ lib/EkiDataJp.pm
@@ -0,0 +1,32 @@
+package EkiDataJp;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+EkiDataJp -
+
+=head1 SYNOPSIS
+
+  use EkiDataJp;
+
+=head1 DESCRIPTION
+
+EkiDataJp is
+
+=head1 AUTHOR
+
+masasuzu / SUZUKI Masashi E<lt>m15.suzuki.masashi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
